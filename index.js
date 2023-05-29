9402fad92cb91d1c719785af70ec29f51ac5f3ef
JackelineGS <jackeline.garcia.serna@gmail.com>
2023-05-29 16:35:18 -0500

    Se está creando options

--- index.js
+++ index.js
@@ -3,15 +3,33 @@ const path = require('path');
 const { validatePath, statusDirectory, statusFile } = require('./paths');
 const { readFile } = require('./readArrays');
 
-const mdLinks = (ruta) => new Promise((resolve, reject) => {
+const mdLinks = (ruta, options) => new Promise((resolve, reject) => {
   const validar = validatePath(ruta);
   if (statusFile(validar) && path.extname(ruta) === '.md') {
     readFile(ruta).then((result) => {
       const contenido = result;
-      console.log(contenido);
       const expresions = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/ig;
       const pathsMd = contenido.match(expresions);
       console.log(pathsMd);
+      const pathResult = pathsMd.map((element) => path.parse(element));
+      console.log(pathResult);
+      if (options === true) {
+        const validateTrue = {
+          href: pathsMd,
+          text: 'Texto que aparece dentro del link (<a>)',
+          file: 'Ruta del archivo donde se encontro el link',
+        };
+        console.log (validateTrue);
+      } else {
+        const validateFalse = {
+          href: pathsMd,
+          text: 'Texto que aparece dentro del link (<a>)',
+          file: 'Ruta del archivo donde se encontro el link',
+          status: 'Código de respuesta HTTP',
+          ok: 'Mensaje fail en caso de fallo u Ok en caso de éxito',
+        };
+        console.log(validateFalse);
+      }
     });
   } else if (statusDirectory(validar)) {
     const directory = fs.readdirSync(ruta);
@@ -27,7 +45,7 @@ const mdLinks = (ruta) => new Promise((resolve, reject) => {
 });
 
 // console.log(mdLinks('./src'));
-mdLinks('./src').then((result) => {
+mdLinks('./src', true).then((result) => {
   console.log(result);
 });
 
